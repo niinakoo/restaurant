93f990190fb3f75182fbf9eca33c50d95860ec7e
Niina <niina.kuusisto@myy.haaga-helia.fi>
2020-10-21 19:14:17 +0300

    Bootstrap lisätty

--- src/main/java/fi/kuusisto/restaurant/web/RestaurantController.java
+++ src/main/java/fi/kuusisto/restaurant/web/RestaurantController.java
@@ -3,15 +3,11 @@ package fi.kuusisto.restaurant.web;
 import java.util.List;
 import java.util.Optional;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
